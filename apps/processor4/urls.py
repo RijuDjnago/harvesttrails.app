1a09b310f700ede9971e2758db67c14683e585e6
RinaProg <nathrina152@gmail.com>
2024-05-28 15:33:55 +0530

    Implemented T3 user

--- apps/processor4/urls.py
+++ apps/processor4/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('inbound_shipment_edit_processor4/<int:pk>/', views.inbound_shipment_edit, name="inbound_shipment_edit4"),
     path('inbound_shipment_delete_processor4/<int:pk>/', views.inbound_shipment_delete_processor4, name="inbound_shipment_delete_processor4"),
     path('receive_shipment_processor4/', views.receive_shipment, name="receive_shipment4"),
+
+    path('inbound_production_management_processor4/', views.inbound_production_management_processor4, name="inbound_production_management_processor4"),
+    path('add_volume_pulled_processor4/', views.add_volume_pulled_processor4, name="add_volume_pulled_processor4"),
+    path('edit_volume_pulled_processor4/<int:pk>/', views.edit_volume_pulled_processor4, name="edit_volume_pulled_processor4"),
+    path('delete_volume_pulled_processor4/<int:pk>/', views.delete_volume_pulled_processor4, name="delete_volume_pulled_processor4"),
+
+    path('processor4_list/', views.processor4_list, name="processor4_list")
 ]
