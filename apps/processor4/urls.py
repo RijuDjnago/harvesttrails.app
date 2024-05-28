368c550aabff6622d2a9710017ca8e8c8825735e
RijuDjnago <rijughosh.claymindsolution@gmail.com>
2024-05-28 13:30:56 +0530

    working on csv download

--- apps/processor4/urls.py
+++ apps/processor4/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('inbound_shipment_view_processor4/<int:pk>/', views.inbound_shipment_view, name="inbound_shipment_view4"),
     path('inbound_shipment_edit_processor4/<int:pk>/', views.inbound_shipment_edit, name="inbound_shipment_edit4"),
     path('receive_shipment_processor4/', views.receive_shipment, name="receive_shipment4"),
+    path('rejected_shipments_csv_download_f4/', views.rejected_shipments_csv_download_for_t4, name="rejected_shipments_csv_download_f4"),
+    path('all_shipments_csv_download_t4/', views.all_shipments_csv_download_for_t4, name="all_shipments_csv_download_t4"),
 ]
